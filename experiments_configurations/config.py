04d993086cf4aa182fb80839e1933e8b0fba3aa5
KimoInaoe <arquimides.mendez@inaoep.mx>
2023-09-21 16:41:12 -0600

    - Refactor in the deeprl experiment config - Create the classes DeepRLConf and DeepCRLConf

--- experiments_configurations/config.py
+++ experiments_configurations/config.py
@@ -98,7 +98,6 @@ class DeepRLConf(AlgConf):
                  final_exploration_rate=0.1, test_exploration_rate=0.05, test_samples=125000,
                  max_no_op_actions=30, alpha_coeff=0.6, n_atoms=51, v_min=-10, v_max=10,
                  n_quantiles=200, n_steps_return=3, sigma_coeff=0.5,
-                 stage_duration=30000, frequency_value=30, confidence_threshold=0.7,
                  use_cuda=False, save=False, load_path=None, render=False, quiet=False, debug=False):
 
         super().__init__(alg_name, combination_strategy)
@@ -139,11 +138,6 @@ class DeepRLConf(AlgConf):
         self.n_steps_return = n_steps_return  # Number of steps for n-step return for Rainbow
         self.sigma_coeff = sigma_coeff  # Sigma0 coefficient for noise initialization
 
-        # CARL related
-        self.stage_duration = stage_duration  # Maximum number of steps in a stage
-        self.frequency_value = frequency_value  # Number of times an action needs to be performed in a state
-        self.confidence_threshold = confidence_threshold  # Confidence threshold in RL-USING-CD stages
-
         # Util
         self.use_cuda = use_cuda  # Flag specifying whether to use the GPU
         self.save = save  # Flag specifying whether to save the model
@@ -151,6 +145,42 @@ class DeepRLConf(AlgConf):
         self.render = render  # Flag specifying whether to render the game
         self.quiet = quiet  # Flag specifying whether to hide the progress bar
         self.debug = debug  # Flag specifying whether the script runs in debug mode
+
+class DeepCRLConf(DeepRLConf):
+    def __init__(self, alg_name, combination_strategy, screen_width=84, screen_height=84,
+                 initial_replay_size=50000, max_replay_size=500000, prioritized=False,
+                 optimizer='adam', learning_rate=0.0001, decay=0.95, epsilon=1e-8,
+                 algorithm='dqn', n_approximators=1, batch_size=32, history_length=4,
+                 target_update_frequency=10000, evaluation_frequency=250000, train_frequency=4,
+                 max_steps=50000000, final_exploration_frame=1000000, initial_exploration_rate=1.0,
+                 final_exploration_rate=0.1, test_exploration_rate=0.05, test_samples=125000,
+                 max_no_op_actions=30, alpha_coeff=0.6, n_atoms=51, v_min=-10, v_max=10,
+                 n_quantiles=200, n_steps_return=3, sigma_coeff=0.5,
+                 use_cuda=False, save=False, load_path=None, render=False, quiet=False, debug=False,
+                 T=30000, th=0.7, min_frequency=30, model_use_strategy=None, model_discovery_strategy=None,
+                 crl_action_selection_strategy=None, use_crl_data=True, model_init_path=None):
+
+        super().__init__(alg_name, combination_strategy, screen_width, screen_height,
+                 initial_replay_size, max_replay_size, prioritized,
+                 optimizer, learning_rate, decay, epsilon,
+                 algorithm, n_approximators, batch_size, history_length,
+                 target_update_frequency, evaluation_frequency, train_frequency,
+                 max_steps, final_exploration_frame, initial_exploration_rate,
+                 final_exploration_rate, test_exploration_rate, test_samples,
+                 max_no_op_actions, alpha_coeff, n_atoms, v_min, v_max,
+                 n_quantiles, n_steps_return, sigma_coeff,
+                 use_cuda, save, load_path, render, quiet, debug)
+
+        # CARL related
+        self.T = T
+        self.th = th
+        self.min_frequency = min_frequency
+        self.model_use_strategy = model_use_strategy
+        self.model_discovery_strategy = model_discovery_strategy
+        self.crl_action_selection_strategy = crl_action_selection_strategy
+        self.use_crl_data = use_crl_data
+        self.model_init_path = model_init_path
+
 
 class RLConf(AlgConf):
     def __init__(self, alg_name, combination_strategy, n, alpha, gamma, epsilon_start, epsilon_end, rl_action_selection_strategy, episode_state_initialization):
@@ -962,7 +992,6 @@ for env_type in EnvironmentType:
 #############################################################
 #                DEEP-RL with CARL experiments              #
 #############################################################
-
 TRIALS = 1
 exp_deep_rl_1 = []
 
@@ -978,11 +1007,10 @@ for env_type in [EnvironmentType.DETERMINISTIC]:
                  final_exploration_rate=0.1, test_exploration_rate=0.05, test_samples=125000,
                  max_no_op_actions=30, alpha_coeff=0.6, n_atoms=51, v_min=-10, v_max=10,
                  n_quantiles=200, n_steps_return=3, sigma_coeff=0.5,
-                 stage_duration=50000000, frequency_value=30, confidence_threshold=0.7,
-                 use_cuda=False, save=False, load_path=None, render=False, quiet=False, debug=False)
+                 use_cuda=True, save=True, load_path=None, render=False, quiet=False, debug=False)
 
     # Here we use the combination strategy 'Discover once, use forever'
-    carl_dqn_conf = DeepRLConf("DQN", combination_strategy=combination_strategy_8, screen_width=84, screen_height=84,
+    carl_dqn_conf = DeepCRLConf("DQN", combination_strategy=combination_strategy_8, screen_width=84, screen_height=84,
                  initial_replay_size=50000, max_replay_size=500000, prioritized=False,
                  optimizer='adam', learning_rate=0.0001, decay=0.95, epsilon=1e-8,
                  algorithm='dqn', n_approximators=1, batch_size=32, history_length=4,
@@ -991,8 +1019,10 @@ for env_type in [EnvironmentType.DETERMINISTIC]:
                  final_exploration_rate=0.1, test_exploration_rate=0.05, test_samples=125000,
                  max_no_op_actions=30, alpha_coeff=0.6, n_atoms=51, v_min=-10, v_max=10,
                  n_quantiles=200, n_steps_return=3, sigma_coeff=0.5,
-                 stage_duration=50000000, frequency_value=30, confidence_threshold=0.7,
-                 use_cuda=False, save=False, load_path=None, render=False, quiet=False, debug=False)
+                 use_cuda=True, save=True, load_path=None, render=False, quiet=False, debug=False,
+                 T=30000, th=0.7, min_frequency=30, model_use_strategy=ModelUseStrategy.POSITIVE_OR_NOT_NEGATIVE,
+                 model_discovery_strategy=ModelDiscoveryStrategy.LESS_SELECTED_ACTION_EPSILON_GREEDY,
+                 crl_action_selection_strategy=ActionSelectionStrategy.MODEL_BASED_EPSILON_GREEDY, use_crl_data=True, model_init_path=None)
 
     experiment = ExpConf("DQN vs CARL-DQN", EnvironmentNames.TAXI_ATARI_SMALL, env_type, TRIALS, 10, EvaluationMetric.EPISODE_REWARD, 50000000, 10000, ActionCountStrategy.Relational, True, [dqn_conf, carl_dqn_conf])
 
